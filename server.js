d4a0f926a0ab3c77e39f777296e678c7230d1c7d
monocrafters <carvistorsllc@gmail.com>
2026-05-07 06:39:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 5000;
-const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000";
+const CLIENT_URL = process.env.CLIENT_URL || "https://zaitoon-royale-frontend.vercel.app";
 
 connectDatabase();
 
